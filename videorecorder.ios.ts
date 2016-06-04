4469e8c2bdfce36f9f9f27afd67a31dbca447d2c
victorsosa <victor.sosa@peopleware.do>
2016-06-04 16:30:58 -0400

    ios issue #5 calling the wrong class method

--- videorecorder.ios.ts
+++ videorecorder.ios.ts
@@ -10,46 +10,46 @@ export class VideoRecorder extends common.VideoRecorder {
 
     record(options: any): Promise<any> {
 		return new Promise((resolve, reject) => {
-		listener = null;
-		var videoEditorController = new UIVideoEditorController();
-		var saveToGallery = true;
-		var quality = 0;
-		var duration = 0.0;
-		if (options) {
-		    saveToGallery = options.saveToGallery ? true : false;
-		    quality = options.hd ? 1 : 0;
-		    duration = options.duration || 0.0;
-		}
+			listener = null;
+			var videoEditorController = new UIVideoEditorController();
+			var saveToGallery = true;
+			var quality = 0;
+			var duration = 0.0;
+			if (options) {
+			    saveToGallery = options.saveToGallery ? true : false;
+			    quality = options.hd ? 1 : 0;
+			    duration = options.duration || 0.0;
+			}
 		
-		if (saveToGallery) {
-		    listener = UIVideoEditorControllerDelegateImpl.new().initWithCallbackAndOptions(resolve, { duration: duration, quality: quality, saveToGallery: saveToGallery });
-		}
-		else {
-		    listener = UIVideoEditorControllerDelegateImpl.new().initWithCallback(resolve);
-		}
-		videoEditorController.delegate = listener;
+			if (saveToGallery) {
+			    listener = UIVideoEditorControllerDelegateImpl.new().initWithCallbackAndOptions(resolve, { duration: duration, quality: quality, saveToGallery: saveToGallery });
+			}
+			else {
+			    listener = UIVideoEditorControllerDelegateImpl.new().initWithCallback(resolve);
+			}
+			videoEditorController.delegate = listener;
 
-		var sourceType = UIImagePickerControllerSourceType.UIImagePickerControllerSourceTypeCamera;
-		var mediaTypes = UIVideoEditorController.availableMediaTypesForSourceType(sourceType);
+			var sourceType = UIImagePickerControllerSourceType.UIImagePickerControllerSourceTypeCamera;
+			var mediaTypes = UIImagePickerController.availableMediaTypesForSourceType(sourceType);
 
-		if (mediaTypes) {
-		    videoEditorController.mediaTypes = mediaTypes;
-		    videoEditorController.sourceType = sourceType;
-		}
+			if (mediaTypes) {
+			    videoEditorController.mediaTypes = mediaTypes;
+			    videoEditorController.sourceType = sourceType;
+			}
 
-		videoEditorController.modalPresentationStyle = UIModalPresentationStyle.UIModalPresentationCurrentContext;
+			videoEditorController.modalPresentationStyle = UIModalPresentationStyle.UIModalPresentationCurrentContext;
 
-		var frame: typeof frameModule = require("ui/frame");
+			var frame: typeof frameModule = require("ui/frame");
 
-		var topMostFrame = frame.topmost();
-		if (topMostFrame) {
-		    var viewController: UIViewController = topMostFrame.currentPage && topMostFrame.currentPage.ios;
-		    if (viewController) {
-		        viewController.presentViewControllerAnimatedCompletion(videoEditorController, true, null);
-		    }
+			var topMostFrame = frame.topmost();
+			if (topMostFrame) {
+			    var viewController: UIViewController = topMostFrame.currentPage && topMostFrame.currentPage.ios;
+			    if (viewController) {
+				viewController.presentViewControllerAnimatedCompletion(videoEditorController, true, null);
+			    }
+			}
 		}
 	}
-    }
 
 }
 
